dfae64ae88b7835d7df95b4833cd34c8d5b44c6a
zombieht <zombie19931113@gmail.com>
2018-07-05 12:39:54 +0800

    fix

--- src/main/java/com/ht/web/spider/Application.java
+++ src/main/java/com/ht/web/spider/Application.java
@@ -1,4 +1,4 @@
-
+package com.ht.web.spider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
+//        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
         SpringApplication.run(Application.class, args);
     }
 }
